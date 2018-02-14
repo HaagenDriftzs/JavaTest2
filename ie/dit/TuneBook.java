3a9e484e56ea0ce3cddfc92ee9bd903021cb9ad4
Thomas Killeen <C16394453@mydit.ie>
2018-02-14 16:54:30 +0000

    I added the code to tune book to open the file and store the information on the file to the array list

--- ie/dit/TuneBook.java
+++ ie/dit/TuneBook.java
@@ -1,6 +1,41 @@
 package ie.dit();
 
+import java.util.ArrayList;
+
 public class TuneBook()
 {
 	ArrayList<String> tunes = new ArrayList<String>();
+	
+	public void TuneBook(String)
+	{
+		BufferedReader inputStream = null;
+		try
+		{
+			inputStream = new BufferedReader(new FileReader("hnj0.abc.txt"));
+			
+			String l;
+			while((l=inputStream.readline())!=null)
+			{
+				tunes.add(l);
+			}
+		}
+		catch(IOException e)
+		{
+			e.printStackTrace();
+		}
+		finally
+		{
+			if(inputStream != null)
+			{
+				try
+				{
+					inputStream.close();
+				}
+				catch(Exception e)
+				{
+					e.printStackTrace();
+				}
+			}
+		}
+	}
 }
